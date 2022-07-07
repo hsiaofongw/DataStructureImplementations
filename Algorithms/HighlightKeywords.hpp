660c30a584fddc6c8d0061dccdddaefd2e206c9c
hsiaofongw <hsiaofong.w@gmail.com>
2022-07-08 02:32:45 +0800

    添加 HighlightKeywords 问题备忘录

---
+++ Algorithms/HighlightKeywords.hpp
@@ -0,0 +1,19 @@
+//
+// Created by 韦晓枫 on 2022/7/8.
+//
+
+#ifndef DATASTRUCTUREIMPLEMENTATIONS_HIGHLIGHTKEYWORDS_HPP
+#define DATASTRUCTUREIMPLEMENTATIONS_HIGHLIGHTKEYWORDS_HPP
+
+#include <string>
+
+namespace Algorithm {
+    namespace HighlightKeywords {
+        struct AnnotatedKeyword {
+            std::string word;
+            bool isHighlighted;
+        };
+    }
+}
+
+#endif //DATASTRUCTUREIMPLEMENTATIONS_HIGHLIGHTKEYWORDS_HPP
