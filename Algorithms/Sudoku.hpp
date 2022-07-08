a41fd16a7d0c046975dfe5a22d6ebc9cf0a6bf18
hsiaofongw <hsiaofong.w@gmail.com>
2022-07-08 23:20:50 +0800

    补充记载数独问题求解算法

--- Algorithms/Sudoku.hpp
+++ Algorithms/Sudoku.hpp
@@ -5,8 +5,15 @@
 #ifndef DATASTRUCTUREIMPLEMENTATIONS_SUDOKU_HPP
 #define DATASTRUCTUREIMPLEMENTATIONS_SUDOKU_HPP
 
-#include <bitset>
 #include <vector>
+#include <iostream>
+#include <bitset>
+#include <list>
+#include <cmath>
+#include <memory>
+#include <array>
+#include <stack>
+
 
 namespace Algorithm {
     /** 合法数独校验算法实现 */
@@ -115,9 +122,158 @@ namespace Algorithm {
     }
 
     /** 数独求解 */
+
     namespace SudokuSolve {
 
+        /** 后续这些子 namespace 会拆分到不同到文件中，故而重复声明。 */
+        struct Point {
+            size_t rowIdx;
+            size_t colIdx;
+        };
+
+        template <size_t N = 9>
+        struct SudokuState {
+            std::array<std::bitset<N>, N> rowBitmaps;
+            std::array<std::bitset<N>, N> colBitmaps;
+            std::array<std::bitset<N>, N> subBoxBitmaps;
+        };
+
+        using Board = std::vector<std::vector<uint8_t>>;
+
+        template <size_t N = 9>
+        bool doSolveSudoku(
+                Board &board,
+                SudokuState<> &state,
+                std::stack<Point> &freeSlots,
+                std::vector<Board> &solutions,
+                bool stopAtFirstSolutionFound
+        ) {
+            if (freeSlots.empty()) {
+                solutions.emplace_back(board);
+                return true;
+            }
+
+            constexpr size_t subBoxIdxMap[N][N] = {
+                    { 0, 0, 0, 1, 1, 1, 2, 2, 2 },
+                    { 0, 0, 0, 1, 1, 1, 2, 2, 2 },
+                    { 0, 0, 0, 1, 1, 1, 2, 2, 2 },
+
+                    { 3, 3, 3, 4, 4, 4, 5, 5, 5 },
+                    { 3, 3, 3, 4, 4, 4, 5, 5, 5 },
+                    { 3, 3, 3, 4, 4, 4, 5, 5, 5 },
+
+                    { 6, 6, 6, 7, 7, 7, 8, 8, 8 },
+                    { 6, 6, 6, 7, 7, 7, 8, 8, 8 },
+                    { 6, 6, 6, 7, 7, 7, 8, 8, 8 },
+            };
+
+            auto testCandidate = [&state, &subBoxIdxMap](const Point &position, const size_t candidate) -> bool {
+                size_t subBoxIdx = subBoxIdxMap[position.rowIdx][position.colIdx];
+                return !(
+                        state.rowBitmaps[position.rowIdx].test(candidate) ||
+                        state.colBitmaps[position.colIdx].test(candidate) ||
+                        state.subBoxBitmaps[subBoxIdx].test(candidate)
+                );
+            };
+
+            auto writeBoard = [&board, &state, &subBoxIdxMap](const Point &position, const size_t digit, bool value) -> void {
+                // digit is 0-based
+                board[position.rowIdx][position.colIdx] = value ? digit + 1 : 0;
+                state.rowBitmaps[position.rowIdx].set(digit, value);
+                state.colBitmaps[position.colIdx].set(digit, value);
+                size_t subBoxIdx = subBoxIdxMap[position.rowIdx][position.colIdx];
+                state.subBoxBitmaps[subBoxIdx].set(digit, value);
+            };
+
+            auto freeSlot = freeSlots.top();
+            for (size_t candidate = 0; candidate < N; ++candidate) {
+                if (testCandidate(freeSlot, candidate)) {
+                    writeBoard(freeSlot, candidate, true);
+                    freeSlots.pop();
+                    bool isValidSolution = doSolveSudoku(board, state, freeSlots, solutions, stopAtFirstSolutionFound);
+                    if (isValidSolution) {
+                        if (stopAtFirstSolutionFound) {
+                            return isValidSolution;
+                        }
+                    }
+                    writeBoard(freeSlot, candidate, false);
+                    freeSlots.push(freeSlot);
+                }
+            }
+
+            return false;
+        }
+
+        template <size_t N = 9>
+        void solveSoduku(Board &board) {
+            SudokuState<> state;
+            std::stack<Point> unFilledSlots;
+            for (size_t i = 0; i < N; ++i) {
+                for (size_t j = 0; j < N; ++j) {
+                    if (board[i][j] == 0) {
+                        Point p {i, j };
+                        unFilledSlots.push(p);
+                    }
+                }
+            }
+
+            std::vector<Board> solutions {};
+            doSolveSudoku<>(board, state, unFilledSlots, solutions, true);
+        }
+
+        std::string boardToStr(const Board &board) {
+            size_t len = 1+3+1+3+1+3+1;
+            char boardView[len][len];
+            for (size_t i = 0; i < len; ++i) {
+                boardView[i][0] = '*';
+                boardView[i][4] = '*';
+                boardView[i][8] = '*';
+                boardView[i][len-1] = '*';
+
+                boardView[0][i] = '*';
+                boardView[4][i] = '*';
+                boardView[8][i] = '*';
+                boardView[len-1][i] = '*';
+            }
+
+            auto digitToChar = [](uint8_t digit) -> char {
+                char digitChars[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                return digitChars[static_cast<size_t>(digit)];
+            };
+
+            auto myMemMap = [&board, &boardView, &digitToChar](size_t fromI, size_t fromJ, size_t toI, size_t toJ) -> void {
+                for (size_t i = 0; i < 3; ++i) {
+                    for (size_t j = 0; j < 3; ++j) {
+                        char c = digitToChar(board[fromI+i][fromJ+j]);
+                        boardView[toI+i][toJ+j] = c;
+                    }
+                }
+            };
+
+            myMemMap(0, 0, 1, 1);
+            myMemMap(0, 3, 1, 5);
+            myMemMap(0, 6, 1, 9);
+            myMemMap(3, 0, 5, 1);
+            myMemMap(3, 3, 5, 5);
+            myMemMap(3, 6, 5, 9);
+            myMemMap(6, 0, 9, 1);
+            myMemMap(6, 3, 9, 5);
+            myMemMap(6, 6, 9, 9);
+
+            std::string result;
+            for (size_t i = 0; i < len; ++i) {
+                std::string row;
+                for (size_t j = 0; j < len; ++j) {
+                    row += boardView[i][j];
+                }
+                row += '\n';
+                result += row;
+            }
+
+            return result;
+        }
     }
+
 }
 
 #endif //DATASTRUCTUREIMPLEMENTATIONS_SUDOKU_HPP
