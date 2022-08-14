e9f3974f96fd48513e8d5e234330ab3534216f60
hsiaofongw <hsiaofong.w@gmail.com>
2022-08-14 15:51:05 +0800

    In-Memory FileSystem: 通过 https://leetcode.com/problems/design-in-memory-file-system/submissions/

--- main.cpp
+++ main.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
 #include "SystemDesign/LFUCache.hpp"
 #include <utility>
+#include "SystemDesign/FileSystem.hpp"
 
 /**
  [[2],[2,1],[3,2],[3],[2],[4,3],[2],[3],[4]]
  */
 
 int main() {
-    using LFUCache = SystemDesign::Cache::LFUCacheGen<>;
-    LFUCache lfuCache (2);
-    lfuCache.printInternal();
-
-    auto put = [&lfuCache](int key, int value) -> void {
-        std::cout << "put: " << key << std::endl;
-        lfuCache.put(key, key);
-        lfuCache.printInternal();
-    };
-    auto get = [&lfuCache](int key) -> void {
-        int val = lfuCache.get(key);
-        std::cout << "get " << key << ": " << val << std::endl;
-        lfuCache.printInternal();
-    };
-
-    put(2,1);
-    put(3,2);
-    get(3);
-    get(2);
-    put(4,3);
-    get(2);
-    get(3);
-    get(4);
+    using SystemDesign::FileSystem::LeetCode::FileSystem;
+    FileSystem fs;
+    fs.ls("/");
+    fs.mkdir("/a/b/c");
+    fs.addContentToFile("/a/b/c/d", "hello");
+    fs.ls("/");
+    auto content = fs.readContentFromFile("/a/b/c/d");
 
     return 0;
 }
